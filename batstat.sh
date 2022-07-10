8f524779c9e5bc8dedfc014e54f5dd3af75a5e97
categorille <categorille@protonmail.com>
2022-07-10 19:07:39 +0200

    fix wrong discharge rate color between 5 and 10 watts

--- batstat.sh
+++ batstat.sh
@@ -3,7 +3,7 @@
 BATTERY_PATH="/sys/class/power_supply/BAT0"
 
 # echoes content of file $1 found in $BATTERY_PATH
-get_battery_info() {
+function get_battery_info {
     if [[ -r $BATTERY_PATH/$1 ]]; then
         echo $(< $BATTERY_PATH/$1)
     fi
@@ -25,25 +25,33 @@ color_clear="\e[0m"
 # first for battery percentage
 color_pct="\e[32m"      # green
 # %.* means remove suffix of the form .*, so that
-# we get values that are valid for bash arithmetic
-if (( ${battery_percent%.*} < 15 )); then
+# we get integer values that are valid for bash arithmetic
+if [[ ${battery_percent%.*} -lt "15" ]]; then
     color_pct="\e[31m"  # red
-elif (( ${battery_percent%.*} < 50 )); then
+elif [[ ${battery_percent%.*} -lt "30" ]]; then
     color_pct="\e[33m"  # brown
 fi
 
-# then for discharge rate, add colors + sign
-color_rate="\e[0m"
+# then for {,dis}charge rate, add colors + sign
 # NOTE : status can be one of Charging, Discharging or Unknown
 if [[ $ac_status == "Charging" ]]; then
-    discharge_rate="+$discharge_rate"
     color_rate="\e[32m" # green
-elif [[ $ac_status == "Discharging" ]]; then
+    discharge_rate="+${discharge_rate}"
+elif [[ "$ac_status" == "Discharging" ]]; then
+    discharge_rate_int="${discharge_rate%.*}"
+    echo "Discharge rate (int): ${discharge_rate_int}"
+    # add colors depending on how much drain there is on the battery
+    if [[ "$discharge_rate_int" -lt "5" ]]; then
+        color_rate="\e[32m" # green
+    elif [[ "$discharge_rate_int" -lt "10" ]] && [[ "$discharge_rate_int" >=5 ]]; then
+        color_rate="\e[33m" # brown
+    else
+        color_rate="\e[31m" # red
+    fi
     discharge_rate="-$discharge_rate"
-    color_rate="\e[31m" # red
-else
+else # Unknown
+    color_rate="\e[34m" # blue
     discharge_rate="?$discharge_rate"
-    color_rate="\e[33m" # brown
 fi
 
 # display formatted result
